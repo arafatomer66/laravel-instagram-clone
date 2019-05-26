c5e8f5457ffd89635d55cff66c0a617e0c3f111e
Arafat <arafatomer66@gmail.com>
2019-05-26 14:56:23 +0600

    initial front end 42:5

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,30 @@
         <div class="col-3 p-5">
              <img style="height:130px;width:130px;" src="/img/logo.png" class="rounded-circle" >
         </div>
-        <div class="col-9">
+        <div class="col-9 pt-5">
              <div><h1>PicShare</h1></div>
+             <div class="d-flex">
+                 <div class="pr-5"><strong>153</strong> posts</div>
+                 <div class="pr-5"><strong>23k</strong> followers</div>
+                 <div class="pr-5"><strong>212</strong> following</div>
+             </div>
+
+             <div class="pt-4 font-weight-bold" > omerarafat.com </div>
+             <div>PicShare is a photo-sharing website that allows everyone to socially connect with other users and view their photos</div>
+             <div> <a href="https://www.omerarafat.com">www.omerarafat.com</a> </div>           
+        </div>
+    </div>
+
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="/img/2.jpg" class="w-100" >
+        </div>
+        <div class="col-4">
+            <img src="/img/2.jpg" class="w-100" >
+        </div>
+        <div class="col-4">
+            <img src="/img/2.jpg" class="w-100" >
         </div>
     </div>
 </div>
