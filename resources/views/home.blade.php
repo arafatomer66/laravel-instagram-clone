8e20736661279428c7e34699b2eea9ba0e5e2373
Arafat <arafatomer66@gmail.com>
2019-05-26 12:11:41 +0600

    34.04

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
 <div class="container">
-   
+    <div class="row">
+        <div class="col-3 p-5">
+             <img style="height:130px;width:130px;" src="/img/logo.png" class="rounded-circle" >
+        </div>
+        <div class="col-9">
+             <div><h1>PicShare</h1></div>
+        </div>
+    </div>
 </div>
 @endsection
