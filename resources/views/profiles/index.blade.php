f04b945367b25449a3127d33ceaf962a8040a7ef
Arafat <arafatomer66@gmail.com>
2019-05-28 12:31:46 +0600

    image resize

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -9,10 +9,10 @@
         <div class="col-9 pt-5">
         <div class="d-flex justify-content-between align-items-baseline" >
             <h1>{{ $user->username }}</h1>
-            <a href="#">Add New Post</a>
+            <a href="/p/create">Add New Post</a>
         </div>
              <div class="d-flex">
-                 <div class="pr-5"><strong>153</strong> posts</div>
+             <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
                  <div class="pr-5"><strong>23k</strong> followers</div>
                  <div class="pr-5"><strong>212</strong> following</div>
              </div>
@@ -25,15 +25,12 @@
 
 
     <div class="row pt-5">
-        <div class="col-4">
-            <img src="/img/2.jpg" class="w-100" >
-        </div>
-        <div class="col-4">
-            <img src="/img/2.jpg" class="w-100" >
-        </div>
-        <div class="col-4">
-            <img src="/img/2.jpg" class="w-100" >
-        </div>
+
+        @foreach ($user->posts as $post)
+        <div class="col-4 pb-4">
+        <img src="/storage/{{ $post->image }}" class="w-100" >
+            </div>
+        @endforeach
     </div>
 </div>
 @endsection
