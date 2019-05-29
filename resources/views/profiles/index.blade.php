a7aeb4978dd52a50771817353f2a4c30ec26aa41
Arafat <arafatomer66@gmail.com>
2019-05-29 11:59:06 +0600

    cont ...

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -9,9 +9,15 @@
         <div class="col-9 pt-5">
         <div class="d-flex justify-content-between align-items-baseline" >
             <h1>{{ $user->username }}</h1>
+            
+            @can('update', $user->profile)
             <a href="/p/create">Add New Post</a>
+        @endcan
         </div>
-        <a href="/profile/{{$user->id}}/edit">Edit Post</a>
+        @can('update', $user->profile)
+            <a href="/profile/{{$user->id}}/edit">Edit Post</a>
+        @endcan
+        
 
              <div class="d-flex">
              <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
