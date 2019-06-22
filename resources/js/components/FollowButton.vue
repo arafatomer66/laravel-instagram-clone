ef6c30e5adb0d34cbe2e15e807e91381a400648c
Md Omer Arafat <arafatomer66@gmail.com>
2019-06-22 14:14:59 +0600

    vue started

---
+++ resources/js/components/FollowButton.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+       <button class="btn btn-primary ml-4" @click="followUser" >Follow</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+        
+
+        methods: {
+            followUser(){
+              alert('works');
+            }
+        },
+    }
+</script>
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -8,8 +8,15 @@
         </div>
         <div class="col-9 pt-5">
         <div class="d-flex justify-content-between align-items-baseline" >
-            <h1>{{ $user->username }}</h1>
-            
+                <div class="d-flex pb-2 align-items-center" >
+
+                        <div class="h4" >{{ $user->username }}</div>
+
+                        {{-- FollowButton Component --}}
+    
+                        <follow-button></follow-button>
+    
+                </div>
             @can('update', $user->profile)
             <a href="/p/create">Add New Post</a>
         @endcan
